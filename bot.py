6144b747899b3422e190d5d5913f27b359a158b0
knmuhammed2@gmail.com <knmuhammed2@gmail.com>
2023-10-28 10:57:20 +0530

    done

--- bot.py
+++ bot.py
@@ -80,7 +80,7 @@ async def req_accept(c, m):
     chat_id = m.chat.id
     if not await Data.find_one({'id': user_id}): await Data.insert_one({'id': user_id})
     await c.approve_chat_join_request(chat_id, user_id)
-    try: await c.send_message(user_id, ACCEPTED_TEXT.format(user=m.from_user.mention, chat=m.chat.tilte))
+    try: await c.send_message(user_id, ACCEPTED_TEXT.format(user=m.from_user.mention, chat=m.chat.title))
     except Exception as e: print(e)
    
    
